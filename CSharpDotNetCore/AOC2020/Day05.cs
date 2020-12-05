644a4e4c3a9c5737a3dfb28d580c46d5cbc1d794
Tony Hunt <thunt@findmypast.com>
2020-12-05 16:00:12 +0000

    Day 5 C# difference of sums solution

--- CSharpDotNetCore/AOC2020/Day05.cs
+++ CSharpDotNetCore/AOC2020/Day05.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AOC2020
@@ -6,44 +7,30 @@ namespace AOC2020
     public static class Day05
     {
         public static int part1(IEnumerable<string> input) => input.Max(decode);
+
         public static int part2(IEnumerable<string> input)
         {
-            var allSeats = input.Select(decode).OrderBy(x => x).ToList();
-            for (var i = 0; i < allSeats.Count-1; i++)
-                if (allSeats[i] + 2 == allSeats[i + 1])
-                    return allSeats[i] + 1;
-            return 0;
+            var allSeats = input.Select(decode).ToList();
+            var sumOfSeats = allSeats.Sum();
+            var min = allSeats.Min();
+            var max = allSeats.Max();
+            var sumOfRange = Enumerable.Range(min, max - min + 1).Sum();
+            return sumOfRange - sumOfSeats;
         }
 
-        private struct Seat
-        {
-            public int Row;
-            public int Col;
-            public int Id() => Row * 8 + Col;
-        }
-        
-        public static int decode (string input) => decodeSeat(input).Id();
+        public static int decode(string input) => decodeSeat(input);
 
-        private static Seat decodeSeat(string input) =>
-            new Seat
-            {
-                Row = decodePart(input.Substring(0, 7)),
-                Col = decodePart(input.Substring(7, 3))
-            };
+        private static int decodeSeat(string input) =>
+            decodePart(input.Substring(0, 7)) * 8 +
+            decodePart(input.Substring(7, 3));
 
-        private static int decodePart(string input)
-        {
-            var n = 1;
-            var x = 0;
-            var stack = new Stack<char>(input.ToCharArray());
-            while (stack.Count>0)
-            {
-                var next = stack.Pop();
-                if (next == 'B' || next == 'R') x += n;
-                n *= 2;
-            }
-
-            return x;
-        }
+        private static int decodePart(string input) =>
+            Convert.ToInt32(
+                input
+                    .Replace("B", "1")
+                    .Replace("R", "1")
+                    .Replace("F", "0")
+                    .Replace("L", "0")
+                , 2);
     }
 }
